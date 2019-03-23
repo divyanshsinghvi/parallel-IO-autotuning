7b39b3ee8c017945459d705b14c765299b69da60
Divyansh Singhvi <divyanshsinghvi@gmail.com>
2019-03-23 17:56:04 +0530

    scripts

---
+++ scripts/ds/mnt.sh
@@ -0,0 +1,1 @@
+sshfs dsinghvi@hpc2010.hpc.iitk.ac.in:/home/dsinghvi/progress /home/dsinghvi/sem8/parallel/project/mnt/
--- scripts/ds/test1.sh
+++ scripts/ds/test1.sh
@@ -1,1 +1,1 @@
-mpiexec -l -n 8  -env PNETCDF_HINTS_DISPLAY=1 -env PNETCDF_HINTS="romio_ds_write=enable;nc_header_align_size=103171^C ./s3d_io.x 100 200 100 2 2 2 1 F output/"
+mpiexec -l -n 8  -env PNETCDF_HINTS_DISPLAY=1 -env PNETCDF_HINTS="romio_ds_write=enable;nc_header_align_size=103171" ./s3d_io.x 100 200 100 2 2 2 1 F output/"
