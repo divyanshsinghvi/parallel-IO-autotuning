931aa19b7430a436bf91ed8de20343cffbd294ab
meghaagr13 <agarwalmegha1308@gmail.com>
2019-04-06 14:53:47 +0530

    making virtual env and installing dependenct

---
+++ progress/regression/run.sh
@@ -0,0 +1,10 @@
+python3 -m pip install --user --upgrade pip
+python3 -m pip install --user virtualenv
+
+cd $HOME/progress
+python3 -m virtualenv env
+
+source env/bin/activate
+pip install numpy
+pip install scipy
+pip install -U scikit-learn
