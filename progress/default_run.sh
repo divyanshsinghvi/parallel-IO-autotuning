dd62c6bb417fb4a7f55c3b28b86471a33697c881
meghaagr13 <agarwalmegha1308@gmail.com>
2019-04-18 02:31:04 +0530

    default benchmarks

---
+++ progress/default_run.sh
@@ -0,0 +1,301 @@
+
+out=$(python3 default_S3D.py -n 2 -c"100 100 100 2 2 4 1")
+out=$(python3 default_S3D.py -n 2 -c"100 100 100 2 2 4 1")
+echo $out
+echo $out >> 'default.txt'
+echo "done"
+rm S3D-IO/output/*    
+
+out=$(python3 default_S3D.py -n 2 -c"100 100 200 2 2 4 1")
+out=$(python3 default_S3D.py -n 2 -c"100 100 200 2 2 4 1")
+echo $out
+echo $out >> 'default.txt'
+echo "done"
+rm S3D-IO/output/*    
+
+out=$(python3 default_S3D.py -n 2 -c"100 200 200 2 2 4 1")
+out=$(python3 default_S3D.py -n 2 -c"100 200 200 2 2 4 1")
+echo $out
+echo $out >> 'default.txt'
+echo "done"
+rm S3D-IO/output/*    
+out=$(python3 default_S3D.py -n 2 -c"200 200 200 2 2 4 1")
+out=$(python3 default_S3D.py -n 2 -c"200 200 200 2 2 4 1")
+echo $out
+echo $out >> 'default.txt'
+echo "done"
+rm S3D-IO/output/*    
+out=$(python3 default_S3D.py -n 2 -c"100 100 400 2 2 4 1")
+out=$(python3 default_S3D.py -n 2 -c"100 100 400 2 2 4 1")
+echo $out
+echo $out >> 'default.txt'
+echo "done"
+rm S3D-IO/output/*    
+
+out=$(python3 default_S3D.py -n 2 -c"100 200 400 2 2 4 1")
+out=$(python3 default_S3D.py -n 2 -c"100 200 400 2 2 4 1")
+echo $out
+echo $out >> 'default.txt'
+echo "done"
+rm S3D-IO/output/*    
+
+
+out=$(python3 default_S3D.py -n 2 -c"200 200 400 2 2 4 1")
+out=$(python3 default_S3D.py -n 2 -c"200 200 400 2 2 4 1")
+echo $out
+echo $out >> 'default.txt'
+echo "done"
+rm S3D-IO/output/*    
+
+
+out=$(python3 default_S3D.py -n 2 -c"200 200 400 2 2 4 1")
+out=$(python3 default_S3D.py -n 2 -c"200 200 400 2 2 4 1")
+echo $out
+echo $out >> 'default.txt'
+echo "done"
+rm S3D-IO/output/*    
+
+out=$(python3 default_S3D.py -n 2 -c"200 400 400 2 2 4 1")
+out=$(python3 default_S3D.py -n 2 -c"200 400 400 2 2 4 1")
+echo $out
+echo $out >> 'default.txt'
+echo "done"
+rm S3D-IO/output/*    
+out=$(python3 default_S3D.py -n 2 -c"100 400 400 2 2 4 1")
+out=$(python3 default_S3D.py -n 2 -c"100 400 400 2 2 4 1")
+echo $out
+echo $out >> 'default.txt'
+echo "done"
+rm S3D-IO/output/*    
+
+out=$(python3 default_S3D.py -n 2 -c"400 400 400 2 2 4 1")
+out=$(python3 default_S3D.py -n 2 -c"400 400 400 2 2 4 1")
+echo $out
+echo $out >> 'default.txt'
+echo "done"
+rm S3D-IO/output/*    
+
+
+out=$(python3 default_S3D.py -n 4 -c"100 100 100 2 4 4 1")
+out=$(python3 default_S3D.py -n 4 -c"100 100 100 2 4 4 1")
+echo $out
+echo $out >> 'default.txt'
+echo "done"
+rm S3D-IO/output/*    
+
+out=$(python3 default_S3D.py -n 4 -c"100 100 200 2 4 4 1")
+out=$(python3 default_S3D.py -n 4 -c"100 100 200 2 4 4 1")
+echo $out
+echo $out >> 'default.txt'
+echo "done"
+rm S3D-IO/output/*    
+
+out=$(python3 default_S3D.py -n 4 -c"100 200 200 2 4 4 1")
+out=$(python3 default_S3D.py -n 4 -c"100 200 200 2 4 4 1")
+echo $out
+echo $out >> 'default.txt'
+echo "done"
+rm S3D-IO/output/*    
+out=$(python3 default_S3D.py -n 4 -c"200 200 200 2 4 4 1")
+out=$(python3 default_S3D.py -n 4 -c"200 200 200 2 4 4 1")
+echo $out
+echo $out >> 'default.txt'
+echo "done"
+rm S3D-IO/output/*    
+out=$(python3 default_S3D.py -n 4 -c"100 100 400 2 4 4 1")
+out=$(python3 default_S3D.py -n 4 -c"100 100 400 2 4 4 1")
+echo $out
+echo $out >> 'default.txt'
+echo "done"
+rm S3D-IO/output/*    
+
+out=$(python3 default_S3D.py -n 4 -c"100 200 400 2 4 4 1")
+out=$(python3 default_S3D.py -n 4 -c"100 200 400 2 4 4 1")
+echo $out
+echo $out >> 'default.txt'
+echo "done"
+rm S3D-IO/output/*    
+
+
+out=$(python3 default_S3D.py -n 4 -c"200 200 400 2 4 4 1")
+out=$(python3 default_S3D.py -n 4 -c"200 200 400 2 4 4 1")
+echo $out
+echo $out >> 'default.txt'
+echo "done"
+rm S3D-IO/output/*    
+
+
+out=$(python3 default_S3D.py -n 4 -c"200 200 400 2 4 4 1")
+out=$(python3 default_S3D.py -n 4 -c"200 200 400 2 4 4 1")
+echo $out
+echo $out >> 'default.txt'
+echo "done"
+rm S3D-IO/output/*    
+
+out=$(python3 default_S3D.py -n 4 -c"200 400 400 2 4 4 1")
+out=$(python3 default_S3D.py -n 4 -c"200 400 400 2 4 4 1")
+echo $out
+echo $out >> 'default.txt'
+echo "done"
+rm S3D-IO/output/*    
+out=$(python3 default_S3D.py -n 4 -c"100 400 400 2 4 4 1")
+out=$(python3 default_S3D.py -n 4 -c"100 400 400 2 4 4 1")
+echo $out
+echo $out >> 'default.txt'
+echo "done"
+rm S3D-IO/output/*    
+
+out=$(python3 default_S3D.py -n 4 -c"400 400 400 2 4 4 1")
+out=$(python3 default_S3D.py -n 4 -c"400 400 400 2 4 4 1")
+echo $out
+echo $out >> 'default.txt'
+echo "done"
+rm S3D-IO/output/*  
+
+  
+
+out=$(python3 default_S3D.py -n 8 -c"100 100 100 4 4 4 1")
+out=$(python3 default_S3D.py -n 8 -c"100 100 100 4 4 4 1")
+echo $out
+echo $out >> 'default.txt'
+echo "done"
+rm S3D-IO/output/*    
+
+out=$(python3 default_S3D.py -n 8 -c"100 100 200 4 4 4 1")
+out=$(python3 default_S3D.py -n 8 -c"100 100 200 4 4 4 1")
+echo $out
+echo $out >> 'default.txt'
+echo "done"
+rm S3D-IO/output/*    
+
+out=$(python3 default_S3D.py -n 8 -c"100 200 200 4 4 4 1")
+out=$(python3 default_S3D.py -n 8 -c"100 200 200 4 4 4 1")
+echo $out
+echo $out >> 'default.txt'
+echo "done"
+rm S3D-IO/output/*    
+out=$(python3 default_S3D.py -n 8 -c"200 200 200 4 4 4 1")
+out=$(python3 default_S3D.py -n 8 -c"200 200 200 4 4 4 1")
+echo $out
+echo $out >> 'default.txt'
+echo "done"
+rm S3D-IO/output/*    
+out=$(python3 default_S3D.py -n 8 -c"100 100 400 4 4 4 1")
+out=$(python3 default_S3D.py -n 8 -c"100 100 400 4 4 4 1")
+echo $out
+echo $out >> 'default.txt'
+echo "done"
+rm S3D-IO/output/*    
+
+out=$(python3 default_S3D.py -n 8 -c"100 200 400 4 4 4 1")
+out=$(python3 default_S3D.py -n 8 -c"100 200 400 4 4 4 1")
+echo $out
+echo $out >> 'default.txt'
+echo "done"
+rm S3D-IO/output/*    
+
+
+out=$(python3 default_S3D.py -n 8 -c"200 200 400 4 4 4 1")
+out=$(python3 default_S3D.py -n 8 -c"200 200 400 4 4 4 1")
+echo $out
+echo $out >> 'default.txt'
+echo "done"
+rm S3D-IO/output/*    
+
+
+out=$(python3 default_S3D.py -n 8 -c"200 200 400 4 4 4 1")
+out=$(python3 default_S3D.py -n 8 -c"200 200 400 4 4 4 1")
+echo $out
+echo $out >> 'default.txt'
+echo "done"
+rm S3D-IO/output/*    
+
+out=$(python3 default_S3D.py -n 8 -c"200 400 400 4 4 4 1")
+out=$(python3 default_S3D.py -n 8 -c"200 400 400 4 4 4 1")
+echo $out
+echo $out >> 'default.txt'
+echo "done"
+rm S3D-IO/output/*    
+out=$(python3 default_S3D.py -n 8 -c"100 400 400 4 4 4 1")
+out=$(python3 default_S3D.py -n 8 -c"100 400 400 4 4 4 1")
+echo $out
+echo $out >> 'default.txt'
+echo "done"
+rm S3D-IO/output/*    
+
+out=$(python3 default_S3D.py -n 8 -c"400 400 400 4 4 4 1")
+out=$(python3 default_S3D.py -n 8 -c"400 400 400 4 4 4 1")
+echo $out
+echo $out >> 'default.txt'
+echo "done"
+rm S3D-IO/output/*  
+
+
+out=$(python3 default_S3D.py -n 8 -c"100 100 200 4 4 8 1")
+out=$(python3 default_S3D.py -n 8 -c"100 100 200 4 4 8 1")
+echo $out
+echo $out >> 'default.txt'
+echo "done"
+rm S3D-IO/output/*    
+
+out=$(python3 default_S3D.py -n 8 -c"100 200 200 4 4 8 1")
+out=$(python3 default_S3D.py -n 8 -c"100 200 200 4 4 8 1")
+echo $out
+echo $out >> 'default.txt'
+echo "done"
+rm S3D-IO/output/*    
+out=$(python3 default_S3D.py -n 8 -c"200 200 200 4 4 8 1")
+out=$(python3 default_S3D.py -n 8 -c"200 200 200 4 4 8 1")
+echo $out
+echo $out >> 'default.txt'
+echo "done"
+rm S3D-IO/output/*    
+out=$(python3 default_S3D.py -n 8 -c"100 100 400 4 4 8 1")
+out=$(python3 default_S3D.py -n 8 -c"100 100 400 4 4 8 1")
+echo $out
+echo $out >> 'default.txt'
+echo "done"
+rm S3D-IO/output/*    
+
+out=$(python3 default_S3D.py -n 8 -c"100 200 400 4 4 8 1")
+out=$(python3 default_S3D.py -n 8 -c"100 200 400 4 4 8 1")
+echo $out
+echo $out >> 'default.txt'
+echo "done"
+rm S3D-IO/output/*    
+
+
+out=$(python3 default_S3D.py -n 8 -c"200 200 400 4 4 8 1")
+out=$(python3 default_S3D.py -n 8 -c"200 200 400 4 4 8 1")
+echo $out
+echo $out >> 'default.txt'
+echo "done"
+rm S3D-IO/output/*    
+
+
+out=$(python3 default_S3D.py -n 8 -c"200 200 400 4 4 8 1")
+out=$(python3 default_S3D.py -n 8 -c"200 200 400 4 4 8 1")
+echo $out
+echo $out >> 'default.txt'
+echo "done"
+rm S3D-IO/output/*    
+
+out=$(python3 default_S3D.py -n 8 -c"200 400 400 4 4 8 1")
+out=$(python3 default_S3D.py -n 8 -c"200 400 400 4 4 8 1")
+echo $out
+echo $out >> 'default.txt'
+echo "done"
+rm S3D-IO/output/*    
+out=$(python3 default_S3D.py -n 8 -c"100 400 400 4 4 8 1")
+out=$(python3 default_S3D.py -n 8 -c"100 400 400 4 4 8 1")
+echo $out
+echo $out >> 'default.txt'
+echo "done"
+rm S3D-IO/output/*    
+
+out=$(python3 default_S3D.py -n 8 -c"400 400 400 4 4 8 1")
+out=$(python3 default_S3D.py -n 8 -c"400 400 400 4 4 8 1")
+echo $out
+echo $out >> 'default.txt'
+echo "done"
+rm S3D-IO/output/*    
