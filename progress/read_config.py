b66e5e1e3911c5cfc40b798954a972415d1b1047
meghaagr13 <agarwalmegha1308@gmail.com>
2019-04-17 21:36:31 +0530

    updated notebook and logs

--- progress/read_config.py
+++ progress/read_config.py
@@ -41,7 +41,7 @@ for key in data["mpi"]:
 
 
 logging.debug("MPI parameters:" + mpi_hints)
-specific_commands="50 50 100 2 2 4 1"
+specific_commands="50 100 100 2 2 4 1"
 logging.debug("S3D-IO :" + specific_commands)
 log = open('../stats.txt','a')
 out=subprocess.Popen(["./run.sh", mpi_hints,specific_commands], shell=False, stdout=subprocess.PIPE)
