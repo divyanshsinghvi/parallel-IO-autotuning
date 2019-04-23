f2039d2d8a84e3e0580c2cfb47aac04b101049aa
Divyansh Singhvi <divyanshsinghvi@gmail.com>
2019-04-23 01:41:17 +0000

    FINAL COMMIT BEFORE REPORT

---
+++ progress/somemoreplots/bt-default-best-plotscript.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+df= pd.read_csv("bt_default.txt",delimiter=" ")
+df['col1'] = df.iloc[:,1]
+df_best= pd.read_csv("xgbtioresult.txt",delimiter=" ")
+df_best['col1'] = df_best.iloc[:,1]
+df = pd.merge(df,df_best,on=['col1'])
+#print(df.iloc[:,1]["200-200-400-4-4-4-1"])
+i = 0
+pd.set_option('display.max_columns', 500)
+print(df)
+axis = df.plot.bar(x='col1',y=[5,15],title="default v/s optimized",figsize=(15,10),legend=True,fontsize=14)
+axis.legend(["write_default","write_XGBoost"],fontsize=14)
+axis.tick_params(axis='x',labelrotation=0, labelsize=11)
+axis.tick_params(axis='y', labelsize=14)
+i = i+1
+plt.savefig("xgboostbtiodefaultvsbest.png")
